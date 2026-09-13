2008a526ca4826e3672b6f7c2d031093d39935da
jamesrenrold-lab <jamesrenrold@gmail.com>
2026-09-13 00:51:21 -0400

    Fix optional CBC projectile type import

--- src/main/java/com/jamesrenrold/elijah/DomainAbilities.java
+++ src/main/java/com/jamesrenrold/elijah/DomainAbilities.java
@@ -23,6 +23,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.projectile.Projectile;
